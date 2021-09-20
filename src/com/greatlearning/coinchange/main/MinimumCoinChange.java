1b78335345196ec91c26d6faa69a56e8627ae44c
siddharthssr94 <87899676+siddharthssr94@users.noreply.github.com>
2021-09-20 15:09:21 +0530

    v1.2
    updated code to capture cases , when coin change is not possible with highest Denomination

--- src/com/greatlearning/coinchange/main/MinimumCoinChange.java
+++ src/com/greatlearning/coinchange/main/MinimumCoinChange.java
@@ -1,34 +1,40 @@
 package com.greatlearning.coinchange.main;
+
 import com.greatlearning.coinchange.services.SortDenominations;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MinimumCoinChange {
     private static Scanner sc = new Scanner(System.in);
 
-    public static void main(String args[]) {
+    //Global Variables
+    public static int denominations[],denominationCount[] , transactionSize, originalAmount;
+
+    //Get inputs from user
+    void getInputs() {
+
         //Get the size of denominations from user
         System.out.println("Enter the size of currency denominations : ");
-        int size = sc.nextInt();
+        transactionSize = sc.nextInt();
         System.out.println();
 
         //Allocate an array of provided size
-        int denominations[] = new int[size];
-        int denominationCount[] = new int[size];
+        denominations = new int[transactionSize];
 
         //Get Denominations from user
         System.out.println("Enter the currency denominations value : ");
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < transactionSize; i++) {
             denominations[i] = sc.nextInt();
         }
-        System.out.println();
 
-        //Sort Deonimation in descending order
-        SortDenominations sd = new SortDenominations();
-        sd.sort(denominations, 0, denominations.length - 1);
+    }
 
-        //Get the amount to be paid from user
-        System.out.println("Enter the amount you want to pay : ");
-        int amount = sc.nextInt();
+    //Calculate remaining amount after coin change and save count of coin for each denomination
+    int getMinimumCoinChange(){
+        //Allocate an array of provided size
+        denominationCount = new int[transactionSize];
+        int amount = originalAmount;
         System.out.println();
 
         // Get Currency count
@@ -40,15 +46,57 @@ public class MinimumCoinChange {
             // set amount to the remaining amount
             amount = amount - (count * denominations[i]);
         }
+        return amount;
+    }
 
-        if (amount != 0) {
-            System.out.println("Given amount is not acheivable with given denominations.");
-        } else {
-            System.out.println("Your payment approach in order to give min no of notes will be : ");
+    //Display denomination and its coin count
+    void displayCoinChange(){
+        System.out.println("Your payment approach in order to give min no of notes will be : ");
             for (int i = 0; i < denominations.length; i++) {
                 if(denominationCount[i] > 0)
                     System.out.println(denominations[i] + " : " + denominationCount[i]);
             }
+    }
+
+    //Driver Class
+    public static void main(String args[]) {
+
+        MinimumCoinChange change = new MinimumCoinChange();
+        change.getInputs();
+
+        //Sort Denomination in descending order
+        SortDenominations sd = new SortDenominations();
+        sd.sort(denominations, 0, denominations.length - 1);
+
+        //Get the amount to be paid from user
+        System.out.println("Enter the amount you want to pay : ");
+        originalAmount = sc.nextInt();
+
+        int size = denominations.length;
+        boolean changePossible = false;
+
+        while(size>0){
+
+            //Get amount left after change
+            int amount = change.getMinimumCoinChange();
+
+            if (amount !=0){
+                //Remove 1st(highest always) denomination from the array
+                denominations = Arrays.copyOfRange(denominations,1,transactionSize);
+                size--;
+                continue;
+            }else{
+                changePossible = true;
+                break;
+            }
+
         }
+
+        //Display Result
+        if(changePossible == false)
+            System.out.println("Given amount is not achievable with given denominations.");
+        else
+            //Display the denomination with count
+            change.displayCoinChange();
     }
 }
